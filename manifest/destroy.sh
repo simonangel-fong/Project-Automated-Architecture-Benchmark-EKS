29b517414bd4e3830a09a6dd969f6402ed2a5e94
simonangel-fong <simonangelfong@gmail.com>
2026-03-01 18:02:56 -0500

    cicd: debug

--- manifest/destroy.sh
+++ manifest/destroy.sh
@@ -12,11 +12,11 @@ echo "# Delete K8s Resource"
 echo "# #################################"
 echo
 
-kubectl delete -f manifest/$ARCH/05_ingress.yaml
-kubectl delete -f manifest/$ARCH/04_app_fastapi.yaml
-kubectl delete -f manifest/$ARCH/03_external_secrets.yaml
-kubectl delete -f manifest/$ARCH/02_cluste_secret_store.yaml
-kubectl delete -f manifest/$ARCH/01_ns.yaml
+kubectl delete -f manifest/$ARCH/05_ingress.yaml --ignore-not-found
+kubectl delete -f manifest/$ARCH/04_app_fastapi.yaml --ignore-not-found
+kubectl delete -f manifest/$ARCH/03_external_secrets.yaml --ignore-not-found
+kubectl delete -f manifest/$ARCH/02_cluste_secret_store.yaml --ignore-not-found
+kubectl delete -f manifest/$ARCH/01_ns.yaml --ignore-not-found
 
 echo
 echo "# #################################"
