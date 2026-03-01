95396a7e0bb06f4b214c2415918a6aaa5b58b291
simonangel-fong <simonangelfong@gmail.com>
2026-03-01 18:04:40 -0500

    cicd: debug

--- manifest/destroy.sh
+++ manifest/destroy.sh
@@ -13,10 +13,6 @@ echo "# #################################"
 echo
 
 kubectl delete -f manifest/$ARCH/05_ingress.yaml --ignore-not-found
-kubectl delete -f manifest/$ARCH/04_app_fastapi.yaml --ignore-not-found
-kubectl delete -f manifest/$ARCH/03_external_secrets.yaml --ignore-not-found
-kubectl delete -f manifest/$ARCH/02_cluste_secret_store.yaml --ignore-not-found
-kubectl delete -f manifest/$ARCH/01_ns.yaml --ignore-not-found
 
 echo
 echo "# #################################"
