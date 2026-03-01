02a146af2314af286144c7e5708837b8957fa8f8
simonangel-fong <simonangelfong@gmail.com>
2026-03-01 18:05:39 -0500

    cicd: debug

--- manifest/destroy.sh
+++ manifest/destroy.sh
@@ -13,13 +13,17 @@ echo "# #################################"
 echo
 
 kubectl delete -f manifest/$ARCH/05_ingress.yaml --ignore-not-found
+# kubectl delete -f manifest/$ARCH/04_app_fastapi.yaml --ignore-not-found
+# kubectl delete -f manifest/$ARCH/03_external_secrets.yaml --ignore-not-found
+# kubectl delete -f manifest/$ARCH/02_cluste_secret_store.yaml --ignore-not-found
+# kubectl delete -f manifest/$ARCH/01_ns.yaml --ignore-not-found
 
-echo
-echo "# #################################"
-echo "# Uninstall Helm packages"
-echo "# #################################"
-echo
+# echo
+# echo "# #################################"
+# echo "# Uninstall Helm packages"
+# echo "# #################################"
+# echo
 
-helm uninstall -n external-dns external-dns 
-helm uninstall -n kube-system aws-load-balancer-controller
-helm uninstall -n external-secrets external-secrets 
+# helm uninstall -n external-dns external-dns 
+# helm uninstall -n kube-system aws-load-balancer-controller
+# helm uninstall -n external-secrets external-secrets 
