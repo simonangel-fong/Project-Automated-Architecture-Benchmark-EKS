dc29a3a6b2900b0cf387011ca54549cc79a51016
simonangel-fong <simonangelfong@gmail.com>
2026-03-01 14:46:20 -0500

    script init.sh: update

--- manifest/init.sh
+++ manifest/init.sh
@@ -77,5 +77,5 @@ kubectl apply -f ./$ARCH/02_cluste_secret_store.yaml
 kubectl apply -f ./$ARCH/03_external_secrets.yaml
 kubectl apply -f ./$ARCH/04_app_fastapi.yaml
 
-sleep 30s
+sleep 30
 kubectl apply -f ./$ARCH/05_ingress.yaml
