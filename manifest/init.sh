a702b760552a295868a74a673ad1fb01b35b55a7
simonangel-fong <simonangelfong@gmail.com>
2026-03-01 20:50:50 -0500

    cicd: debug

--- manifest/init.sh
+++ manifest/init.sh
@@ -96,3 +96,4 @@ kubectl apply -f manifest/$ARCH/02_cluste_secret_store.yaml
 kubectl apply -f manifest/$ARCH/03_external_secrets.yaml
 kubectl apply -f manifest/$ARCH/04_app_fastapi.yaml
 kubectl apply -f manifest/$ARCH/05_ingress.yaml
+kubectl apply -f manifest/$ARCH/06_hpa.yaml
