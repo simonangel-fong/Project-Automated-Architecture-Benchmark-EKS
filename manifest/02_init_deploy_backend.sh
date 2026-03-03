4bcf9f8241a017e3c1d14e2349d61c240bdb48fa
simonangel-fong <simonangelfong@gmail.com>
2026-03-02 19:47:48 -0500

    script: update

--- manifest/02_init_deploy_backend.sh
+++ manifest/02_init_deploy_backend.sh
@@ -3,8 +3,6 @@
 
 set -Eeuo pipefail
 
-DIR_MANIFEST="manifest/$ARCH"
-
 echo
 echo "# #################################"
 echo "#  Apply Application"
@@ -18,13 +16,10 @@ echo
 # kubectl apply -f manifest/$ARCH/05_ingress.yaml
 # kubectl apply -f manifest/$ARCH/06_hpa.yaml
 
-# kubectl create -f manifest/$ARCH/01_ns.yaml 2>/dev/null || kubectl replace -f manifest/$ARCH/01_ns.yaml
-# kubectl create -f manifest/$ARCH/02_cluste_secret_store.yaml 2>/dev/null || kubectl replace -f manifest/$ARCH/02_cluste_secret_store.yaml
-# kubectl create -f manifest/$ARCH/03_external_secrets.yaml 2>/dev/null || kubectl replace -f manifest/$ARCH/03_external_secrets.yaml
-# kubectl create -f manifest/$ARCH/04_app_fastapi.yaml 2>/dev/null || kubectl replace -f manifest/$ARCH/04_app_fastapi.yaml
-# kubectl create -f manifest/$ARCH/05_ingress.yaml 2>/dev/null || kubectl replace -f manifest/$ARCH/05_ingress.yaml
-# kubectl create -f manifest/$ARCH/06_hpa.yaml 2>/dev/null || kubectl replace -f manifest/$ARCH/06_hpa.yaml
+kubectl create -f manifest/$ARCH/01_ns.yaml 2>/dev/null || kubectl replace -f manifest/$ARCH/01_ns.yaml
+kubectl create -f manifest/$ARCH/02_cluste_secret_store.yaml 2>/dev/null || kubectl replace -f manifest/$ARCH/02_cluste_secret_store.yaml
+kubectl create -f manifest/$ARCH/03_external_secrets.yaml 2>/dev/null || kubectl replace -f manifest/$ARCH/03_external_secrets.yaml
+kubectl create -f manifest/$ARCH/04_app_fastapi.yaml 2>/dev/null || kubectl replace -f manifest/$ARCH/04_app_fastapi.yaml
+kubectl create -f manifest/$ARCH/05_ingress.yaml 2>/dev/null || kubectl replace -f manifest/$ARCH/05_ingress.yaml
+kubectl create -f manifest/$ARCH/06_hpa.yaml 2>/dev/null || kubectl replace -f manifest/$ARCH/06_hpa.yaml
 
-for file in $DIR_MANIFEST; do
-    echo "$file"
-done
