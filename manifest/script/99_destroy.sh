9936ac4b41d41781a993c7a3f5d62c214ca64159
simonangel-fong <simonangelfong@gmail.com>
2026-03-09 16:50:04 -0400

    debug cicd

--- manifest/script/99_destroy.sh
+++ manifest/script/99_destroy.sh
@@ -10,4 +10,5 @@ echo
 
 # remove 
 kubectl delete -f manifest/backend/$ARCH/99_ingress.yaml --ignore-not-found
+kubectl delete -f manifest/backend/$ARCH/03_app_fastapi.yaml --ignore-not-found
 kubectl delete -f manifest/backend/$ARCH/01_karpenter.yaml --ignore-not-found
