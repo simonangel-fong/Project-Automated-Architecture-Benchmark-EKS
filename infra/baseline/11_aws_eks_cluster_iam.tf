ccd036e08b58d72312a9ef60b91493a7f681b1ef
simonangel-fong <simonangelfong@gmail.com>
2026-03-09 02:23:36 -0400

    cicd: debug

--- infra/baseline/11_aws_eks_cluster_iam.tf
+++ infra/baseline/11_aws_eks_cluster_iam.tf
@@ -1,65 +1,104 @@
-# aws_eks_access.tf
+# aws_eks_cluster_iam.tf
 
 locals {
-  access_entries = {
-    cluster_admin = {
-      # principal_arn = "${aws_iam_role.eks_admin_access.arn}"
-      principal_arn = var.cluster_admin_arn
-      policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-      scope         = "cluster"
-      namespaces    = []
-      description   = "Human: daily ops and break-glass"
-    }
-    github_cicd = {
-      principal_arn = var.github_cicd_role_arn
-      # policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
-      policy_arn  = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-      scope       = "cluster"
-      namespaces  = []
-      description = "CI/CD App pipeline: Helm add-ons and application deployments"
-    }
+  oidc_provider = replace(module.eks.cluster_oidc_issuer_url, "https://", "")
+  # oidc_provider_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${module.eks.oidc_provider}"
+}
 
-    # developer = {
-    #   principal_arn = var.developer_role_arn
-    #   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSEditPolicy"
-    #   scope         = "namespace"
-    #   namespaces    = var.app_namespaces # e.g. ["app-backend", "app-frontend"]
-    #   description   = "Human: app debugging and workload management, namespace-scoped"
-    # }
-    # auditor = {
-    #   principal_arn = var.auditor_role_arn
-    #   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
-    #   scope         = "cluster"
-    #   namespaces    = []
-    #   description   = "Human: compliance and reporting, read-only"
-    # }
+# #############################################
+# IAM Role: SA for AWS Load Balancer Controller
+# #############################################
+# role of Load Balancer Controller
+resource "aws_iam_role" "lbc" {
+  name = "${module.eks.cluster_name}-lbc"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        # Federated = local.oidc_provider_arn
+        Federated = module.eks.oidc_provider_arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${local.oidc_provider}:aud" = "sts.amazonaws.com"
+          "${local.oidc_provider}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+        }
+      }
+    }]
+  })
+}
+
+# policy for LBC
+resource "aws_iam_policy" "lbc" {
+  name   = "${module.eks.cluster_name}-lbc"
+  policy = file("${path.module}/../share/iam_policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "lbc" {
+  role       = aws_iam_role.lbc.name
+  policy_arn = aws_iam_policy.lbc.arn
+}
+
+# #############################################
+# IAM Role: SA for ESO
+# #############################################
+# role of External Secrets Operator
+resource "aws_iam_role" "eso" {
+  name = "${module.eks.cluster_name}-eso"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        # Federated = local.oidc_provider_arn
+        Federated = module.eks.oidc_provider_arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${local.oidc_provider}:aud" = "sts.amazonaws.com"
+          "${local.oidc_provider}:sub" = "system:serviceaccount:external-secrets:external-secrets"
+        }
+      }
+    }]
+  })
+}
+
+# policy for External Secrets Operator
+data "aws_iam_policy_document" "eso" {
+  # Allow listing/batch on all secrets (needed for certain workflows/features)
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:ListSecrets",
+      "secretsmanager:BatchGetSecretValue"
+    ]
+    resources = ["*"]
+  }
+
+  # Allow read-only access to a scoped set of secrets
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetResourcePolicy",
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:ListSecretVersionIds"
+    ]
+    resources = [aws_secretsmanager_secret.app.arn]
   }
 }
 
-resource "aws_eks_access_entry" "eks" {
-  for_each = local.access_entries
-
-  cluster_name  = module.eks.cluster_name
-  principal_arn = each.value.principal_arn
-  type          = "STANDARD"
-
-  tags = {
-    Description = each.value.description
-    ManagedBy   = "terraform"
-  }
+resource "aws_iam_policy" "eso" {
+  name   = "${var.project_name}-${var.arch}-eso"
+  policy = data.aws_iam_policy_document.eso.json
 }
 
-resource "aws_eks_access_policy_association" "eks" {
-  for_each = local.access_entries
-
-  cluster_name  = module.eks.cluster_name
-  principal_arn = each.value.principal_arn
-  policy_arn    = each.value.policy_arn
-
-  access_scope {
-    type       = each.value.scope
-    namespaces = each.value.scope == "namespace" ? each.value.namespaces : []
-  }
-
-  depends_on = [aws_eks_access_entry.eks]
+resource "aws_iam_role_policy_attachment" "eso" {
+  role       = aws_iam_role.eso.name
+  policy_arn = aws_iam_policy.eso.arn
 }
